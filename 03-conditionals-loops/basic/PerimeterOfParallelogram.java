dba7c5c07abae92614d6e7059ef6e1ed39b30e06
mohit deshwal <mohitdeshwal1999@gmail.com>
2021-10-05 21:01:58 +0530

    Update PerimeterOfParallelogram.java
    your program wasn't printing perimeter of parallelogram it was just add all the input and store in perimeter.
    formula of perimeter of parallelogram is 2(a+b)
    so i've changed some lines and it's working fine now :)

--- 03-conditionals-loops/basic/PerimeterOfParallelogram.java
+++ 03-conditionals-loops/basic/PerimeterOfParallelogram.java
@@ -6,14 +6,11 @@ import java.util.Scanner;
 public class PerimeterOfParallelogram {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        float perimeter = 0;
-        int count = 1;
-        while (count != 5)
-        {
-            System.out.print("Enter side " +count+ " of parallelogram: ");
-            perimeter += in.nextFloat();
-            count += 1;
-        }
+            System.out.print("Enter opposie side1 of parallelogram: ");
+            Double side1 = in.nextDouble();
+            System.out.print("Enter opposie side2 of parallelogram: ");
+            Double side2 = in.nextDouble();
+            Double perimeter = 2*(side1+side2);
         System.out.println("Perimeter of Parallelogram: "+perimeter);
     }
 }

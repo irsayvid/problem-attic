47cc592e3d6d6fcde1da757908b8adfa7d4f2d82
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2022-02-02 20:42:47 +0530

    Updated Factorial.java

--- 02-first-java/Factorial.java
+++ 02-first-java/Factorial.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.println("Enter a number: ");
         int n = in.nextInt();
         System.out.println("Factorial of " + n + " is " + factorial(n));
     }

ccd3a850ad41e11baac8df37fc4c5778639c4689
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2022-02-02 20:44:48 +0530

    Updated Greeting.java

--- 02-first-java/Greeting.java
+++ 02-first-java/Greeting.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Greeting {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter your name: ");
+        System.out.println("Enter your name: ");
         String name = in.nextLine();
         System.out.println("Hey " + name + "!");
     }
